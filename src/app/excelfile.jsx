ee48cfbf4070416c1ac82bb3d869d6003203944d
Devy Salomon <devysalomon@gmail.com>
2024-08-23 17:05:58 -0500

    cambios en las anotaciones y comentarios

--- src/app/excelfile.jsx
+++ src/app/excelfile.jsx
@@ -94,7 +94,28 @@ function Excelfile() {
         await workbook.xlsx.load(data);
         const worksheet = workbook.worksheets[0];
 
-        const columnIndices = [5, 6, 7, 8, 10, 13, 14, 15, 16, 25, 26, 27];
+        const columnIndices = [
+          5, // Fecha de emision
+          6, // fecha de venc
+          7, // tipo cp
+          8, // serie de cdp
+          10, // numero cp
+          13, // numero de identidad
+          14, // apellidos y nombres
+          15, // BI Gravado DG
+          16, // IGV / IPM DG
+          25, // total cp
+          26, // moneda
+          27, // tipo de cambio
+          21, // valor adquirido
+          24, // otros trib
+          17, // BI Gravado DGNG
+          18, // IGV / IPM DGNG
+          19, // BI Gravado DNG
+          20, // IGV / IPM DNG
+          22, // ISC
+          23, // ICBPER
+        ];
         const startRow = 2;
 
         const values = [];
@@ -146,72 +167,250 @@ function Excelfile() {
 
     let campo = 1;
     let subComp = 1;
+    let rows = [];
     columnData.forEach((rowValues) => {
       const subCompFormatted = String(subComp).padStart(4, "0");
       let divisionResult = ((rowValues[8] / rowValues[7]) * 100).toFixed(0);
       let igvValue =
         divisionResult === "18" ? "18" : divisionResult === "10" ? "10" : "";
+      // Verificar y agregar row1
+      if (rowValues[7] !== 0) {
+        const row1 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[7] / rowValues[11]).toFixed(2))
+            : rowValues[7], // BI Gravado DG
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row1);
+      }
+      // Verificar y agregar row2
+      if (rowValues[8] !== 0) {
+        const row2 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[8] / rowValues[11]).toFixed(2))
+            : rowValues[8], // IGV / IPM DG
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row2);
+      }
+      // Verificar y agregar row3
+      if (rowValues[14] !== 0) {
+        const row3 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[14] / rowValues[11]).toFixed(2))
+            : rowValues[14], // BI Gravado DGNG
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row3);
+      }
+      // Verificar y agregar row4
+      if (rowValues[15] !== 0) {
+        const row4 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[15] / rowValues[11]).toFixed(2))
+            : rowValues[15], // IGV / IPM DGNG
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row4);
+      }
+      // Verificar y agregar row5
+      if (rowValues[16] !== 0) {
+        const row5 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[16] / rowValues[11]).toFixed(2))
+            : rowValues[16], // BI Gravado DNG
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row5);
+      }
+      // Verificar y agregar row6
+      if (rowValues[17] !== 0) {
+        const row6 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[17] / rowValues[11]).toFixed(2))
+            : rowValues[17], // IGV / IPM DNG
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row6);
+      }
+      // Verificar y agregar row7
+      if (rowValues[12] !== 0) {
+        const row7 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[12] / rowValues[11]).toFixed(2))
+            : rowValues[12], // valor adquirido
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row7);
+      }
+      // Verificar y agregar row8
+      if (rowValues[18] !== 0) {
+        const row8 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[18] / rowValues[11]).toFixed(2))
+            : rowValues[18], // ISC
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row8);
+      }
+      // Verificar y agregar row9
+      if (rowValues[19] !== 0) {
+        const row9 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
+          //...rowValues.slice(5, 6)
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[19] / rowValues[11]).toFixed(2))
+            : rowValues[19], // ICBPER
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row9);
+      }
+      if (rowValues[13] !== 0) {
+        const row10 = [
+          campo,
+          11,
+          `${selectedMonth}${subCompFormatted}`,
+          rowValues[0],
+          rowValues[0],
+          codigoMap[rowValues[2]] || rowValues[2],
+          `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
 
-      const row1 = [
-        campo,
-        11,
-        `${selectedMonth}${subCompFormatted}`,
-        rowValues[0],
-        rowValues[0],
-        codigoMap[rowValues[2]] || rowValues[2],
-        `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
-        //...rowValues.slice(5, 6)
-        rowValues[5],
-        rowValues[6].substring(0, 40), //nombre
-        rowValues[7] === 0
-          ? parseFloat((rowValues[9] / rowValues[11]).toFixed(2))
-          : rowValues[10] === "USD"
-          ? parseFloat((rowValues[7] / rowValues[11]).toFixed(2))
-          : rowValues[7],
-        "D",
-        rowValues[10] === "PEN" ? "MN" : "US",
-        igvValue,
-      ];
-
-      const row2 = [
-        campo,
-        11,
-        `${selectedMonth}${subCompFormatted}`,
-        rowValues[0],
-        rowValues[0],
-        codigoMap[rowValues[2]] || rowValues[2],
-        `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`,
-        //...rowValues.slice(5, 6)
-        rowValues[5],
-        rowValues[6].substring(0, 40), //nombre
-        rowValues[10] === "USD"
-          ? parseFloat((rowValues[8] / rowValues[11]).toFixed(2))
-          : rowValues[8],
-        "D",
-        rowValues[10] === "PEN" ? "MN" : "US",
-        igvValue,
-      ];
-
-      const row3 = [
+          rowValues[5],
+          rowValues[6].substring(0, 40), //nombre
+          rowValues[10] === "USD"
+            ? parseFloat((rowValues[13] / rowValues[11]).toFixed(2))
+            : rowValues[13], // otros tributos
+          "D",
+          rowValues[10] === "PEN" ? "MN" : "US",
+          igvValue,
+        ];
+        rows.push(row10);
+      }
+      // Verificar y agregar row10
+      const row11 = [
         campo, //campo
         11, //subdiario
-        `${selectedMonth}${subCompFormatted}`, // numero de comprobante
-        rowValues[0], // fecha de emision
+        `${selectedMonth}${subCompFormatted}`, // número de comprobante
+        rowValues[0], // fecha de emisión
         rowValues[0], // fecha de vencimiento
         codigoMap[rowValues[2]] || rowValues[2], // tipo cp
         `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`, // serie
-        //...rowValues.slice(5, 6)
         rowValues[5],
-        rowValues[6].substring(0, 40), //nombre
+        rowValues[6].substring(0, 40), // nombre
         rowValues[10] === "USD"
           ? parseFloat((rowValues[9] / rowValues[11]).toFixed(2))
-          : rowValues[9], // monto
+          : rowValues[9], // monto del total
         "H", // debe y haber
         rowValues[10] === "PEN" ? "MN" : "US", // moneda
         igvValue,
       ];
-
-      const rows = [row1, row2, row3];
+      rows.push(row11);
 
       // Alterna colores cada 3 filas
       const isEvenGroup = campo % 2 === 0;
@@ -231,6 +430,7 @@ function Excelfile() {
 
       campo++;
       subComp++;
+      rows.length = 0;
     });
 
     // Obtener la fecha y hora actual
@@ -434,3 +634,72 @@ function Excelfile() {
 }
 
 export default Excelfile;
+
+//rows = [row1, row2, row3, row4, row5, row6, row7, row8, row9, row10];
+
+// if (rowValues[12] === 0 && rowValues[13] === 0) {
+//   const row3 = [
+//     campo, //campo
+//     11, //subdiario
+//     `${selectedMonth}${subCompFormatted}`, // número de comprobante
+//     rowValues[0], // fecha de emisión
+//     rowValues[0], // fecha de vencimiento
+//     codigoMap[rowValues[2]] || rowValues[2], // tipo cp
+//     `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`, // serie
+//     rowValues[5],
+//     rowValues[6].substring(0, 40), // nombre
+//     rowValues[10] === "USD"
+//       ? parseFloat((rowValues[9] / rowValues[11]).toFixed(2))
+//       : rowValues[9], // monto
+//     "H", // debe y haber
+//     rowValues[10] === "PEN" ? "MN" : "US", // moneda
+//     igvValue,
+//   ];
+
+//   rows = [row1, row2, row3];
+// } else {
+// const row3 = [
+//   campo, //campo
+//   11, //subdiario
+//   `${selectedMonth}${subCompFormatted}`, // número de comprobante
+//   rowValues[0], // fecha de emisión
+//   rowValues[0], // fecha de vencimiento
+//   codigoMap[rowValues[2]] || rowValues[2], // tipo cp
+//   `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`, // serie
+//   rowValues[5],
+//   rowValues[6].substring(0, 40), // nombre
+//   rowValues[10] === "USD"
+//     ? parseFloat(
+//         (
+//           (rowValues[12] === 0 ? rowValues[13] : rowValues[12]) /
+//           rowValues[11]
+//         ).toFixed(2)
+//       )
+//     : rowValues[12] === 0
+//     ? rowValues[13]
+//     : rowValues[12], // monto dependiendo de si alguno de los valores no es cero
+//   "D", // debe y haber
+//   rowValues[10] === "PEN" ? "MN" : "US", // moneda
+//   igvValue,
+// ];
+
+// const row4 = [
+//   campo, //campo
+//   11, //subdiario
+//   `${selectedMonth}${subCompFormatted}`, // número de comprobante
+//   rowValues[0], // fecha de emisión
+//   rowValues[0], // fecha de vencimiento
+//   codigoMap[rowValues[2]] || rowValues[2], // tipo cp
+//   `${rowValues[3]}-${String(rowValues[4]).padStart(8, "0")}`, // serie
+//   rowValues[5],
+//   rowValues[6].substring(0, 40), // nombre
+//   rowValues[10] === "USD"
+//     ? parseFloat((rowValues[9] / rowValues[11]).toFixed(2))
+//     : rowValues[9], // monto del total
+//   "H", // debe y haber
+//   rowValues[10] === "PEN" ? "MN" : "US", // moneda
+//   igvValue,
+// ];
+
+// rows = [row1, row2, row3, row4];
+// }
